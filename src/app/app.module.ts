0ba77c20a5c782a3693eccf0242ffcc57d3685a1
cotsell <cotsell@gmail.com>
2018-03-04 19:58:06 +0900

    첫 라우터 설정 전 백업. 아마도 컴포넌트 생성 하고 지워야 하는 경우가 발생할 여지가 있어서 하는 백업.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 
+const route: Routes = [
+  // { path: '', component: ''}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
