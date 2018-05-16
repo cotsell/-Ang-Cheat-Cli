46b0a9888d08a45822d850e76204bf2f11458185
cotsell <cotsell@gmail.com>
2018-05-16 23:22:36 +0900

    누락된 소소한 버그..라기 보다는 변경점 수정.

--- src/app/components/scrap-modal/scrap-modal.component.ts
+++ src/app/components/scrap-modal/scrap-modal.component.ts
@@ -4,7 +4,7 @@ import { Network } from '../../service/Network';
 import { Store } from '@ngrx/store';
 import * as Redux from '../../service/redux';
 import { Subscription } from 'rxJs';
-import { unSubscribe } from '../../service/utils';
+import { unSubscribe, bodyScroll } from '../../service/utils';
 
 @Component({
   selector: 'app-scrap-modal',
@@ -18,9 +18,9 @@ export class ScrapModalComponent implements OnInit, OnDestroy, OnChanges {
   @Output() makeScrap = new EventEmitter<any>();
   @Output() exit = new EventEmitter<boolean>();
 
-  private account =  { logged: false, accessToken: undefined };
-  private scrapList: Scrap;
-  private isMakeNewOpen = false;
+  account =  { logged: false, accessToken: undefined };
+  scrapList: Scrap;
+  isMakeNewOpen = false;
 
   private accountSubc: Subscription;
 
@@ -32,10 +32,11 @@ export class ScrapModalComponent implements OnInit, OnDestroy, OnChanges {
   ngOnInit() {
     if (this.on) {
       this.subscribeAccount();
+      bodyScroll(false);
     }
   }
 
-  private subscribeAccount() {
+  subscribeAccount() {
     this.accountSubc = this.store.select(Redux.getAccount)
       .subscribe(result => {
         if (result !== undefined) {
@@ -50,70 +51,70 @@ export class ScrapModalComponent implements OnInit, OnDestroy, OnChanges {
       });
   }
 
-    // 로그인이 된게 확실했을때 다음을 실행하기 위해 사용하는 함수.
-    private afterCheckingAccount() {
-        this.getScrapList();
+  // 로그인이 된게 확실했을때 다음을 실행하기 위해 사용하는 함수.
+  afterCheckingAccount() {
+    this.getScrapList();
+  }
+
+  // 로그인 한 유저의 스크랩 리스트를 가져와요.
+  getScrapList() {
+    this.network.getScrap(this.account.accessToken)
+    .subscribe(result => {
+      if (result.result === true) {
+        console.log(result.msg);
+        this.scrapList = Object.assign({}, result.payload);
+      } else {
+        console.error(result.msg);
+      }
+    });
+  }
+
+  // 스크랩 저장하기 함수.
+  // label값이 들어오면 기존 라벨을 이용한 스크랩이고,
+  // label값이 없으면, 새로 라벨을 만들어서 스크랩하는 거로 처리해요.
+  makeScrapBtn(event, label?: string) {
+    if (event) { event.stopPropagation(); }
+
+    let labelObj: any;
+
+    if (label) {
+      // 기존 라벨 이용해서 만들기.
+      labelObj = {
+        isNew: false,
+        documentId: this.document.historyId,
+        documentTitle: this.document.title,
+        label: label
+      };
+
+    } else {
+      // 새로 만들기
+      labelObj = {
+        isNew: true,
+        documentId: this.document.historyId,
+        documentTitle: this.document.title,
+        label: this.labelName.nativeElement.value
+      };
     }
 
-    // 로그인 한 유저의 스크랩 리스트를 가져와요.
-    private getScrapList() {
-        this.network.getScrap(this.account.accessToken)
-        .subscribe(result => {
-            if (result.result === true) {
-                console.log(result.msg);
-                this.scrapList = Object.assign({}, result.payload);
-            } else {
-                console.error(result.msg);
-            }
-        });
-    }
+    if (labelObj.label === undefined || labelObj.label === '') {
+      alert(`라벨 이름은 공백을 허용하지 않아요.`);
+    } else {
 
-    // 스크랩 저장하기 함수.
-    // label값이 들어오면 기존 라벨을 이용한 스크랩이고,
-    // label값이 없으면, 새로 라벨을 만들어서 스크랩하는 거로 처리해요.
-    private makeScrapBtn(event, label?: string) {
-        if (event) { event.stopPropagation(); }
-
-        let labelObj: any;
-
-        if (label) {
-            // 기존 라벨 이용해서 만들기.
-            labelObj = {
-                isNew: false,
-                documentId: this.document.historyId,
-                documentTitle: this.document.title,
-                label: label
-            };
-
+      this.network.setScrap(this.account.accessToken, labelObj)
+      .subscribe(result => {
+        if (result.result === true) {
+          console.log(result.msg);
+          this.closeModal(undefined);
         } else {
-            // 새로 만들기
-            labelObj = {
-                isNew: true,
-                documentId: this.document.historyId,
-                documentTitle: this.document.title,
-                label: this.labelName.nativeElement.value
-            };
+          console.error(result.msg);
+          alert(result.msg);
         }
-
-        if (labelObj.label === undefined || labelObj.label === '') {
-            alert(`라벨 이름은 공백을 허용하지 않아요.`);
-        } else {
-
-            this.network.setScrap(this.account.accessToken, labelObj)
-                .subscribe(result => {
-                    if (result.result === true) {
-                        console.log(result.msg);
-                        this.closeModal(undefined);
-                    } else {
-                        console.error(result.msg);
-                        alert(result.msg);
-                    }
-                });
-        }
+      });
     }
+  }
 
   // 새로운 라벨 만들기 항목 화면에 표시하기, 말기.
-  private changeMakeNewState(event) {
+  changeMakeNewState(event) {
     if (event) { event.stopPropagation(); }
 
     this.isMakeNewOpen = !this.isMakeNewOpen;
@@ -130,14 +131,15 @@ export class ScrapModalComponent implements OnInit, OnDestroy, OnChanges {
   }
 
   // on 상태가 false면 정지상태이므로
-  private pause() {
+  pause() {
     unSubscribe(this.accountSubc);
     this.scrapList = undefined;
     this.account = undefined;
     this.isMakeNewOpen = false;
+    bodyScroll(true);
   }
 
-  private closeModal(event) {
+  closeModal(event) {
     if (event) { event.stopPropagation(); }
 
     this.exit.emit(true);
